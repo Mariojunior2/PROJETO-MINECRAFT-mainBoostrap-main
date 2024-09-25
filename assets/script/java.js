e1c89d3c9a7e8771430290ab850a28b67e1d3c7d
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-09-25 15:00:25 -0300

    a
    Co-authored-by: Rodrigo <rodrigosantiagosilva@users.noreply.github.com>
    Co-authored-by: Rafael <Rafaelcstrauss@users.noreply.github.com>

--- assets/script/java.js
+++ assets/script/java.js
@@ -21,6 +21,25 @@ readMoreButtons.forEach((button) => {
 });
 
 
+// CORES DE VIADO
+/*
+function CoresAletor() {
+  const letras = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+      color += letras[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+function MudarCores() {
+  const elements = document.querySelectorAll('.credit *:not(style)');
+  
+  elements.forEach(element => {
+      element.style.color = CoresAletor();
+  });
+}
 
+setInterval(MudarCores, 275);
+*/
 
